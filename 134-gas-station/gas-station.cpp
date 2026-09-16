69907cb7781d0d10020c1035900debe0aba47b21
pari-216 <pari216garg@gmail.com>
2026-09-16 18:22:02 +0530

    Time: 0 ms (100.00%) | Memory: 112.7 MB (13.90%) - LeetSync

---
+++ 134-gas-station/gas-station.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+     int n=gas.size(),tank=0, start=0, total=0;
+     for(int i=0;i<n;i++){
+        int diff=gas[i]-cost[i];
+        tank+=diff;
+        total+=diff;
+        if(tank<0){
+            start=i+1;
+            tank=0;
+        }
+     }
+     if(total<0) return -1;
+     return start;   
+    }
+};
